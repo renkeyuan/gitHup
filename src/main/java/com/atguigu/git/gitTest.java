757ef00cd974c560db22e0326c43bc64902deef8
Renkeyuan <18438100889@163.com>
2021-10-28 15:58:00 +0800

    Hot-fix commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -15,5 +15,6 @@ public class gitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Second Git");
+        System.out.println("hot-fix Git");
     }
 }
