8ca24c8c05cee663d1a6e9fa0135cffc27a04860
Renkeyuan <18438100889@163.com>
2021-10-28 15:53:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * ${Description}
+ *
+ * @author RenKeYuan on 2021/10/28
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
